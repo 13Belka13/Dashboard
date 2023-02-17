124ecf8911cff9a817d5693aa362676e3e96640c
TerokyKun <maxklindyhov2@gmail.com>
2023-02-17 22:33:58 +0300

    input-serch

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -1,10 +1,49 @@
-import { Flex, Heading } from "@chakra-ui/react";
-
-
+import {
+  CardBody,
+  Card,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Flex,
+  Spacer,
+  InputRightElement
+} from "@chakra-ui/react";
+import { SearchIcon } from "@chakra-ui/icons";
 export const Home = () => (
-    <>
-        <Flex height="100vh" alignItems="center" justifyContent="center">
-            <Heading>Install Chakra UI In React JS</Heading>
+  <>
+    <Card    bg="
+     #F5F5F5" borderRadius="0">
+      <CardBody
+        bg="
+      #FFFFFF"
+      margin="60px"
+      borderRadius="20px"
+      >
+        <Flex>
+       
+          <InputGroup size="md">
+          <Spacer/>
+          <InputRightElement>
+              <SearchIcon color="#8A62D7" />
+            </InputRightElement>
+
+            <Input
+              color="#808080"
+              placeholder="Поиск"
+              _placeholder={{ opacity: 0.4, color: "#808080" }}
+              bg="#FFFFFF"
+              w="350px"
+              h="40px"
+              borderRadius="30px"
+              borderColor="#8A62D7"
+              _hover={{ bg: "#FFFFFF", color: "#8A62D7" }}
+              focusBorderColor="pink.400"
+              errorBorderColor="crimson"
+            />
+            
+          </InputGroup>
         </Flex>
-    </>
+      </CardBody>
+    </Card>
+  </>
 );
