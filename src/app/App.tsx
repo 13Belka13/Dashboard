b7d66c86247a2650ea88e868dd05ca86a349217c
TerokyKun <maxklindyhov2@gmail.com>
2023-02-18 00:07:18 +0300

    h1

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,7 +7,7 @@ import { Home } from './pages/Home';
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/">
-      <Route index element={<Home />} />
+      <Route index element={<Home  />} />
     </Route>
   )
 );
--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -6,24 +6,60 @@ import {
   InputLeftElement,
   Flex,
   Spacer,
-  InputRightElement
+  InputRightElement,
+  Heading,
 } from "@chakra-ui/react";
 import { SearchIcon } from "@chakra-ui/icons";
+import { ReactComponent as MenuIcon } from "../../assets/menu.svg";
 export const Home = () => (
   <>
-    <Card    bg="
-     #F5F5F5" borderRadius="0">
+    <Card
+      bg="
+     #F5F5F5"
+      borderRadius="0"
+    >
       <CardBody
         bg="
       #FFFFFF"
-      margin="60px"
-      borderRadius="20px"
+        margin="60px"
+        borderRadius="20px"
       >
         <Flex>
-       
+          <CardBody
+            bg="#8A62D7"
+            h="1100px"
+            w="100px"
+            marginLeft="-5"
+            marginTop="-5"
+          ></CardBody>
+
+          <MenuIcon />
           <InputGroup size="md">
-          <Spacer/>
-          <InputRightElement>
+            <Heading as="h1" size="2xl" color="#8A62D7" mt="60" ml="30">
+              Местоположение / карта
+            </Heading>
+            <Heading
+              as="h1"
+              size="4xl"
+              color="#C2C9D4"
+              mt="60"
+              ml="30"
+              opacity={0.5}
+              sx={{
+                WebkitTextFillColor: "transparent",
+                WebkitTextStroke: "1px rgba(0, 0, 0, 1)",
+                position: "absolute",
+                left: "2vw",
+                top: "1vw",
+                transform: "translateY(-50%)",
+              }}
+              zIndex="-1"
+            >
+              Местоположение / карта
+            </Heading>
+
+            <Spacer />
+            <InputRightElement>
               <SearchIcon color="#8A62D7" />
             </InputRightElement>
 
@@ -40,7 +76,6 @@ export const Home = () => (
               focusBorderColor="pink.400"
               errorBorderColor="crimson"
             />
-            
           </InputGroup>
         </Flex>
       </CardBody>
