5de2bc34b3b751c7c76c17dc3e2963d188dbec79
sonia13marker <soniamarker13@gmail.com>
2023-02-18 01:45:27 +0300

    изменяемый компонент заголовка

--- src/app/@shared/components/DoubleHeader.tsx
+++ src/app/@shared/components/DoubleHeader.tsx
@@ -1,27 +1,34 @@
 import { Heading } from "@chakra-ui/react";
 
-export function DoubleHeader () {
-   return <> <Heading as="h1" size="2xl" color="#8A62D7" mt="60" ml="30">
-    Местоположение / карта
-  </Heading>
-  <Heading
-    as="h1"
-    size="4xl"
-    color="#C2C9D4"
-    mt="60"
-    ml="30"
-    opacity={0.5}
-    sx={{
-      WebkitTextFillColor: "transparent",
-      WebkitTextStroke: "1px rgba(0, 0, 0, 1)",
-      position: "absolute",
-      left: "2vw",
-      top: "1vw",
-      transform: "translateY(-50%)",
-    }}
-    zIndex="-1"
-  >
-    Местоположение / карта
-  </Heading>
-  </>
+interface Props {
+  text: string;
 }
+
+export function DoubleHeader({ text }: Props) {
+  return (<>
+      <Heading as="h1" size="2xl" color="#8A62D7" mt="60" ml="30">
+        {text}
+      </Heading>
+
+      <Heading
+        as="h1"
+        size="4xl"
+        color="#C2C9D4"
+        mt="60"
+        ml="30"
+        opacity={0.5}
+        sx={{
+          WebkitTextFillColor: "transparent",
+          WebkitTextStroke: "1px rgba(0, 0, 0, 1)",
+          position: "absolute",
+          left: "2vw",
+          top: "1vw",
+          transform: "translateY(-50%)",
+        }}
+        zIndex="-1"
+      >
+        {text}
+      </Heading>
+
+      </>) 
+      }
--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -38,26 +38,7 @@ export const Home = () => (
           <MenuIcon />
           <InputGroup size="md">
  
- <DoubleHeader/>
-            <Heading
-              as="h1"
-              size="4xl"
-              color="#C2C9D4"
-              mt="60"
-              ml="30"
-              opacity={0.5}
-              sx={{
-                WebkitTextFillColor: "transparent",
-                WebkitTextStroke: "1px rgba(0, 0, 0, 1)",
-                position: "absolute",
-                left: "2vw",
-                top: "1vw",
-                transform: "translateY(-50%)",
-              }}
-              zIndex="-1"
-            >
-              Местоположение / карта
-            </Heading>
+    <DoubleHeader text={"Ghbdtn"}/>
 
             <Spacer />
             <InputRightElement>
