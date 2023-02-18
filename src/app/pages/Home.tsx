0b49fa81f96bc0ddd7d8ca6ebadd6d178642bbab
TerokyKun <maxklindyhov2@gmail.com>
2023-02-18 03:00:34 +0300

    map-menu

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -8,11 +8,19 @@ import {
   Spacer,
   InputRightElement,
   Heading,
-  Box
+  Box,
+  ButtonGroup,
+  Button,
+  IconButton,
+  Text,
+  Checkbox,
 } from "@chakra-ui/react";
 import { SearchIcon } from "@chakra-ui/icons";
 import { ReactComponent as MenuIcon } from "../../assets/menu.svg";
 import { DoubleHeader } from "../@shared/components/DoubleHeader";
+import { ReactComponent as Map } from "../../assets/Map.svg";
+import { AddIcon } from "@chakra-ui/icons";
+import { MinusIcon } from "@chakra-ui/icons";
 export const Home = () => (
   <>
     <Card
@@ -27,18 +35,9 @@ export const Home = () => (
         borderRadius="20px"
       >
         <Flex>
-          <CardBody
-            bg="#8A62D7"
-            h="1100px"
-            w="100px"
-            marginLeft="-5"
-            marginTop="-5"
-          ></CardBody>
-
           <MenuIcon />
           <InputGroup size="md">
- 
-    <DoubleHeader text={"Ghbdtn"}/>
+            <DoubleHeader text={"Местоположение / карта"} />
 
             <Spacer />
             <InputRightElement>
@@ -59,19 +58,110 @@ export const Home = () => (
               errorBorderColor="crimson"
             />
           </InputGroup>
-          
-   
-      
         </Flex>
-        <Flex bg="#F7F7F7">
-            <Box p='4' color="black">
+        <Flex>
+          <Box>
+            <Box position="relative">
+              <ButtonGroup
+                size="sm"
+                isAttached
+                variant="outline"
+                position="absolute"
+                top="50"
+                left="50"
+                transform="translate(-50%, -50%)"
+                zIndex="1"
+              >
+                <IconButton
+                  aria-label="Add to friends"
+                  icon={<AddIcon />}
+                  borderColor="#D4EF00"
+                  color="#D4EF00"
+                  bg="#FFFFFF"
+                  _hover={{ bg: "#FFFFFF", color: "#8A62D7" }}
+                />
+                <IconButton
+                  aria-label="Add to friends"
+                  icon={<MinusIcon />}
+                  borderColor="#D4EF00"
+                  color="#D4EF00"
+                  bg="#FFFFFF"
+                  _hover={{ bg: "#FFFFFF", color: "#8A62D7" }}
+                />
+              </ButtonGroup>
+
+              <Box zIndex="-1" mt="100">
+      
+              </Box>
+            </Box>
+          </Box>
+          <Spacer />
+          <Box mt="100" paddingRight="60" boxShadow='dark-lg' p='6' rounded='md'>
+            <Box>
+              <Text fontSize="md" color="black">
+                Регион:
+              </Text>
+              <Input
+                color="#808080"
+                placeholder="Выберите регион"
+                _placeholder={{ opacity: 0.4, color: "#808080" }}
+                bg="#FFFFFF"
+                w="350px"
+                h="40px"
+                borderRadius="30px"
+                borderColor="#D4EF00"
+                _hover={{ bg: "#FFFFFF", color: "#8A62D7" }}
+                focusBorderColor="#D4EF00"
+                errorBorderColor="#D4EF00"
+              />
+            </Box>
+            <Checkbox size="md" colorScheme="red" color="#BCBCBE" isInvalid mt="1">
+                Смотреть все
+              </Checkbox>
+            <Box mt="10">
+              <Text fontSize="md" color="black">
+                Город:
+              </Text>
+              <Input
+                color="#808080"
+                placeholder="Выберите город"
+                _placeholder={{ opacity: 0.4, color: "#808080" }}
+                bg="#FFFFFF"
+                w="350px"
+                h="40px"
+                borderRadius="30px"
+                borderColor="#D4EF00"
+                _hover={{ bg: "#FFFFFF", color: "#8A62D7" }}
+                focusBorderColor="#D4EF00"
+                errorBorderColor="#D4EF00"
+              />
+            </Box>
+
+            <Box mt="10">
+              <Text fontSize="md" color="black" mb="10">
+                Выбранное:
+              </Text>
+              <Text fontSize="md" color="black"  mb="10">
+                Ещё ничего не выбрано.
+              </Text>
+                <Button bg="#8A62D7" color="#D4EF00"  _hover={{ opacity:"80%"}} >
+              Сравнить
+            </Button>
+            </Box>
+
+          
+          </Box>
+        </Flex>
+
+        <Flex bg="#F7F7F7" mt="100">
+          <Box p="4" color="black">
             Выполнено командой Frogs в рамках хакатона “Молодёжный бит”
-            </Box>
-            <Spacer />
-            <Box p='4' color="black">
+          </Box>
+          <Spacer />
+          <Box p="4" color="black">
             @2023 год
-            </Box>
-          </Flex>
+          </Box>
+        </Flex>
       </CardBody>
     </Card>
   </>
